ab30a1a0f1d1d47fa5e2edd32756486cf7d66717
Leyder Quintero <lele.quinter@gmail.com>
2022-03-15 22:06:40 -0500

    Agregando estilos al NavLink

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -5,26 +5,24 @@ import { NavLink } from "react-router-dom";
 
 function Category() {
   return (
-    <div>
-      <List>
-        <NavLink to={'/cuisine/italian'}>
-          <FaPizzaSlice />
-          <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/american'}>
-          <FaHamburger />
-          <h4>American</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/thai'}>
-          <GiNoodles />
-          <h4>Thai</h4>
-        </NavLink>
-        <NavLink to={'/cuisine/japanese'}>
-          <GiChopsticks />
-          <h4>Japanese</h4>
-        </NavLink >
-      </List>
-    </div>
+    <List>
+      <SLink to={"/cuisine/italian"}>
+        <FaPizzaSlice />
+        <h4>Italian</h4>
+      </SLink>
+      <SLink to={"/cuisine/american"}>
+        <FaHamburger />
+        <h4>American</h4>
+      </SLink>
+      <SLink to={"/cuisine/thai"}>
+        <GiNoodles />
+        <h4>Thai</h4>
+      </SLink>
+      <SLink to={"/cuisine/japanese"}>
+        <GiChopsticks />
+        <h4>Japanese</h4>
+      </SLink>
+    </List>
   );
 }
 
@@ -32,6 +30,43 @@ const List = styled.div`
   display: flex;
   justify-content: center;
   margin: 2rem 0rem;
+`;
+
+const SLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-left: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  height: 6rem;
+  width: 6rem;
+  cursor: pointer;
+  transform: scale(0.8);
+
+  h4 {
+    color: white;
+    font-size: 0.8rem;
+  }
+
+  svg {
+    color: white;
+    font-size: 2.5rem;
+  }
+
+  &.active {
+    background: linear-gradient(to right, #f27121, #e94057);
+
+    svg {
+      color: white;
+    }
+
+    h4 {
+      color: white;
+    }
+  }
 `;
 
 export default Category;
