4d3225f644304279307ab4464f82364b9dce7085
Leyder Quintero <lele.quinter@gmail.com>
2022-03-16 21:41:18 -0500

    Creacion del Search y de la page Searched

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Category from "./Components/Category";
 import Pages from "./pages/Pages";
+import Search from "./Components/Search";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Category />
         <Pages />
       </BrowserRouter>
